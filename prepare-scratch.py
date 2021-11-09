0ab4159bfc98b6fd1e854535507ec54f2745fb72
Carolyn Davis <carolyn.m.davis01@gmail.com>
2021-11-08 20:51:06 -0700

    prep exerc

---
+++ prepare-scratch.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 20:49:12 2021
+
+@author: carolyndavis
+"""
+
+# =============================================================================
+# PrepaExercises 
+# =============================================================================
+# =============================================================================
+# Create a spark data frame that contains your favorite programming languages.
+# 
+# The name of the column should be language
+# View the schema of the dataframe
+# Output the shape of the dataframe
+# Show the first 5 records in the dataframe
+# =============================================================================
